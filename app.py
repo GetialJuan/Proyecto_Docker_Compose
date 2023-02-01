bb12735f62456768c353b93646c580b2278941fd
GetialJuan <getial.juan@correounivalle.edu.co>
2023-02-01 12:06:10 -0500

    Codigo Funcional (Se puede mejorar con aborts y manejo de errores)

--- app.py
+++ app.py
@@ -1,26 +1,42 @@
 import time
 
-from flask import Flask
+from flask import Flask, jsonify, request
 import psycopg2
 
-intento = True
-while intento:
-    try:
-        conn = psycopg2.connect(database="postgres",
-                                user="postgres",
-                                password="postgres",
-                                host="postgres",
-                                port="5432")
-        intento = False
-        print("Database connected successfully")
-    except:
-        print("Database NOT connected successfully")
-        intento = True
+try:
+    conn = psycopg2.connect(database="postgres",
+                            user="postgres",
+                            password="postgres",
+                            host="postgres",
+                            port="5432")
+    print("Database connected successfully")
+except:
+    print("Database NOT connected successfully")
 
 app = Flask(__name__)
 db = conn.cursor()
 
-@app.route('/')
-def hello():
-    db.execute("SELECT * FROM contador")
-    return 'Contador: {}'.format(db.fetchone())
+@app.route('/', methods=['GET'])
+def getContador():
+    db.execute("UPDATE contador SET contador = (contador + 1)") # Aumenta el contador
+    db.execute("SELECT * FROM contador") # Se obtiene el contador
+    return 'Contador: {}'.format(db.fetchone()[0]) # Se muestra el contador
+
+@app.route('/', methods=['DELETE'])
+def deleteContador():
+    db.execute("DELETE FROM contador") # Eliminamos el contador
+    db.execute("INSERT INTO contador (contador) VALUES (0)") # Insertamos un nuevo contador en 0
+    return jsonify({'result': True})
+
+@app.route('/<int:valor>', methods=['PUT'])
+def updateContador(valor):
+    db.execute("UPDATE contador SET contador = {}".format(valor)) # Actualizamos el contador
+    db.execute("SELECT * FROM contador") # Se obtiene el contador
+    return jsonify({'contador': db.fetchone()[0]})
+
+@app.route('/', methods=['POST'])
+def addContador():
+    newContador = request.json['contador'] # Se recibe el contador nuevo
+    db.execute("DELETE FROM contador") # Se elimina el contador actual
+    db.execute("INSERT INTO contador (contador) VALUES ({})".format(newContador)) # Se añade el nuevo contador
+    return jsonify({'contador': newContador})
